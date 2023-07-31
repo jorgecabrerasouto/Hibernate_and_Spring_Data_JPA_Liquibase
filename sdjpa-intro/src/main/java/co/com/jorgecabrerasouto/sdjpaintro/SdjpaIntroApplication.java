825e172e2c43a51dca45d9fc825819bb32f1be08
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-07-31 14:41:22 -0500

    Initial Commit

---
+++ sdjpa-intro/src/main/java/co/com/jorgecabrerasouto/sdjpaintro/SdjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package co.com.jorgecabrerasouto.sdjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaIntroApplication.class, args);
+	}
+
+}
---
+++ sdjpa-intro/src/main/java/co/com/jorgecabrerasouto/sdjpaintro/domain/Book.java
@@ -0,0 +1,75 @@
+package co.com.jorgecabrerasouto.sdjpaintro.domain;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String title;
+	private String isbn;
+	private String publisher;
+	
+	public Book() {
+		super();
+	}
+	
+	public Book(String title, String isbn, String publisher) {
+		super();
+		this.title = title;
+		this.isbn = isbn;
+		this.publisher = publisher;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	
+
+}
